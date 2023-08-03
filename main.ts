fc1b39cc0a3323d0aa53d6c40e7ebecafff57cdd
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-08-03 10:00:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,23 @@
 input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(45)
+})
+function Uhel_0_cstupnu () {
     basic.showString("0")
     servos.P0.setAngle(0)
     basic.pause(1000)
     basic.clearScreen()
-})
+}
 input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(135)
+})
+function uhel_90_stupnu () {
     basic.showString("1")
     servos.P0.setAngle(90)
     basic.pause(1000)
     basic.clearScreen()
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    servos.P0.setAngle(90)
 })
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
